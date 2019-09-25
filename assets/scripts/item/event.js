17c557265fe9a83bdf9ea2da75ecbb1a2aad9169
elinag <elina.gorshkova@icloud.com>
2019-09-25 15:53:41 -0400

    Add event function that triggers the ajax request
    Sends data from the from end to delete item or returns an error

--- assets/scripts/item/event.js
+++ assets/scripts/item/event.js
@@ -11,6 +11,15 @@ const onGetItems = (event) => {
     .catch(console.error)
 }
 
+const onDeleteItem = (event) => {
+  event.preventDefault()
+  // Should get an id of the resourse that was targeted. At least did it in my other project.
+  const id = $(event.target).closest('section').data('id')
+  api.deleteItem(id)
+  .then(ui.deleteItemSuccess)
+  .catch(console.error)
+}
+
 module.exports = {
   onGetItems
 }
